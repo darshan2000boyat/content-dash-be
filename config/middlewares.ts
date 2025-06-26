8ee063c932a3306e2d1bc3feef971da3456a8d4c
Abhishek <abhishek@tentwenty.me>
2025-06-26 18:53:38 +0530

    base plugins for emails and uploads

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,6 +2,33 @@ export default [
   "strapi::logger",
   "strapi::errors",
   "strapi::security",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          "default-src": ["'self'", "data:", "blob:", "https:", "http:"],
+          "script-src": [
+            "'self'",
+            "'unsafe-inline'",
+            "https://editor.unlayer.com",
+          ],
+          "style-src": ["'self'", "'unsafe-inline'", "https:", "http:"],
+          "img-src": ["'self'", "data:", "blob:", "https:", "http:"],
+          "media-src": ["'self'", "data:", "blob:", "https:", "http:"],
+          "connect-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "https:",
+            "http:",
+            "wss:",
+          ],
+          "font-src": ["'self'", "https:", "data:"],
+        },
+      },
+    },
+  },
   "strapi::cors",
   "strapi::poweredBy",
   "strapi::query",
--- config/plugins.ts
+++ config/plugins.ts
@@ -1,5 +1,13 @@
-export default ({env}) => ({
-    // ..other plugins
+const LOCAL_UPLOAD_PROVIDER = "local";
+const AWS_S3_UPLOAD_PROVIDER = "aws-s3";
+const SENDMAIL_EMAIL_PROVIDER = "sendmail";
+const NODEMAILER_EMAIL_PROVIDER = "nodemailer";
+const SENDGRID_EMAIL_PROVIDER = "sendgrid";
+
+export default ({ env }) => ({
+  // ..other plugins
+  upload: getUploadProviderConfig(env),
+  email: getEmailProviderConfig(env),
   "strapi-plugin-cron": {
     enabled: true,
   },
@@ -30,3 +38,83 @@ export default ({env}) => ({
     },
   },
 });
+
+const getBaseUploadConfig = () => ({
+  config: {
+    provider: LOCAL_UPLOAD_PROVIDER,
+  },
+});
+
+const getBaseEmailConfig = () => ({
+  config: {
+    provider: SENDMAIL_EMAIL_PROVIDER,
+    providerOptions: {},
+    settings: {},
+  },
+});
+
+const getAWSProviderOptions = (env) => ({
+  baseUrl: env("CDN_URL"),
+  rootPath: env("CDN_ROOT_PATH"),
+  s3Options: {
+    credentials: {
+      accessKeyId: env("AWS_ACCESS_KEY_ID"),
+      secretAccessKey: env("AWS_ACCESS_SECRET"),
+    },
+    region: env("AWS_REGION"),
+    params: {
+      ACL: env("AWS_ACL", "public-read"),
+      signedUrlExpires: env("AWS_SIGNED_URL_EXPIRES", 15 * 60),
+      Bucket: env("AWS_BUCKET"),
+    },
+  },
+});
+
+const getUploadProviderConfig = (env) => {
+  if (env("AWS_PROVIDER") === "true") {
+    return {
+      config: {
+        provider: AWS_S3_UPLOAD_PROVIDER,
+        providerOptions: getAWSProviderOptions(env),
+        actionOptions: {
+          upload: {},
+          uploadStream: {},
+          delete: {},
+        },
+      },
+    };
+  }
+  return getBaseUploadConfig();
+};
+
+const getNodemailerOptions = (env) => ({
+  host: env("SMTP_HOST", "smtp.example.com"),
+  port: env("SMTP_PORT", 587),
+  auth: {
+    user: env("SMTP_USERNAME"),
+    pass: env("SMTP_PASSWORD"),
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
+
+const getEmailProviderConfig = (env) => {
+  const config = getBaseEmailConfig();
+
+  if (env("EMAIL_PROVIDER") === NODEMAILER_EMAIL_PROVIDER) {
+    config.config.provider = NODEMAILER_EMAIL_PROVIDER;
+    config.config.providerOptions = getNodemailerOptions(env);
+  } else if (env("EMAIL_PROVIDER") === SENDGRID_EMAIL_PROVIDER) {
+    config.config.provider = SENDGRID_EMAIL_PROVIDER;
+    config.config.providerOptions = { apiKey: env("SENDGRID_API_KEY") };
+  }
+
+  config.config.settings = {
+    defaultFrom: env("EMAIL_DEFAULT_FROM"),
+    defaultReplyTo: env("EMAIL_DEFAULT_TO"),
+    testAddress: env("EMAIL_DEFAULT_TO"),
+  };
+
+  return config;
+};
